9676904e6ba60c6b9c573667a203625e658c3e1c
Deriandri <deriandriananda82@gmail.com>
2025-06-26 06:54:46 -0700

    Update ohp.sh

--- sshws/ohp.sh
+++ sshws/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
- wget -O /usr/local/bin/ohpserver "https://raw.githubusercontent.com/scblackmarket/VIP/main/sshws/ohpserver"
+ wget -O /usr/local/bin/ohpserver "https://raw.githubusercontent.com/Deriandri/suryafa/main/sshws/ohpserver"
 chmod +x /usr/local/bin/ohpserver
 
 # Installing Service
