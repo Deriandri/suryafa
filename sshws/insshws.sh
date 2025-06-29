150044811bd501aff43e82c069854e97d1dfa7af
Deriandri <deriandriananda82@gmail.com>
2025-06-29 10:25:13 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -5,13 +5,13 @@ file_path="/etc/handeling"
 # Cek apakah file ada
 if [ ! -f "$file_path" ]; then
     # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "𝙎𝙪𝙧𝙮𝙖𝙘𝙤𝙣𝙛𝙞𝙜\nGreen" | sudo tee "$file_path" > /dev/null
+    echo -e "surya config\nGreen" | sudo tee "$file_path" > /dev/null
     echo "File '$file_path' berhasil dibuat."
 else
     # Jika file ada, cek apakah isinya kosong
     if [ ! -s "$file_path" ]; then
         # Jika file ada tetapi kosong, isi dengan dua baris
-        echo -e "𝙨𝙪𝙧𝙮𝙖𝙘𝙤𝙣𝙛𝙞𝙜\nGreen" | sudo tee "$file_path" > /dev/null
+        echo -e "surya config\nGreen" | sudo tee "$file_path" > /dev/null
         echo "File '$file_path' kosong dan telah diisi."
     else
         # Jika file ada dan berisi data, tidak lakukan apapun
